45e89da1f317ba08188ebf4a24ca1df1ee3c8d6a
Ashish Gour <ashish.gour@newvision-software.com>
2023-12-05 16:03:50 +0530

    completed the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+//import { Component } from "react";
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
 
-function App() {
+const App = () => {
+  const [searchField, setSearchField] = useState("");
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => setMonsters(users));
+  }, []);
+
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+    setFilteredMonsters(newFilteredMonsters);
+  }, [searchField, monsters]);
+
+  const onSearchChange = (event) => {
+    setSearchField(event.target.value.toLocaleLowerCase());
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="app-title">Monsters Rolodex</h1>
+      <SearchBox
+        onSearchChange={onSearchChange}
+        placeholder="search monster"
+        className="monsters-search-box"
+      />
+      <CardList monsters={filteredMonsters} />
     </div>
   );
-}
-
+};
 export default App;
+
+//--> below code is done by using class component in react
+
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       monsters: [],
+//       searchField: '',
+//     };
+//   }
+//   componentDidMount() {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then((response) => response.json())
+//       .then((users) => this.setState({ monsters: users }));
+//   }
+
+//   onSearchChange = (event) => {
+//     this.setState({
+//       searchField: event.target.value.toLocaleLowerCase(),
+//     });
+//   }
+
+//   render() {
+//     const {monsters , searchField} = this.state;
+//     const {onSearchChange} = this;
+//     const filteredMonsters = monsters.filter((monster) => {
+//       return monster.name.toLocaleLowerCase().includes(searchField);
+//     });
+//     return (
+//       <div className="App">
+//         <h1 className="app-title">Monsters Rolodex</h1>
+//         <SearchBox onSearchChange = {onSearchChange} placeholder = 'search monster' className = 'monsters-search-box'/>
+//         <CardList monsters = {filteredMonsters}/>
+//       </div>
+//     );
+//   }
+// }
+//export default App;
